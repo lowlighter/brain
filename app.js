94672b270c8b1494081f62fc832441f4ce57f2ca
Lecoq Simon <simon.lecoq@live.fr>
2018-02-09 20:11:01 +0100

    Server can now be started with npm start from root directory

---
+++ app.js
@@ -0,0 +1,28 @@
+//Dependancies
+  const { spawnSync } = require('child_process')
+  const path = require('path')
+  const fs = require('fs')
+  const ls = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+
+//Ignored folders
+  const ignored = [".git", "miscelleanous"]
+
+//Verifying installation
+  process.stdout.write('\x1Bc')
+  console.log("Checking installed modules :")
+  ls(".").filter(f => ignored.indexOf(f) < 0).forEach(f => {
+    //Initialization
+      const cwd = path.join(__dirname, f)
+      const name = `${f.charAt(0).toLocaleUpperCase()}${f.slice(1)}`
+    //Checking modules and install them
+      process.stdout.write(`    \x1b[33m${name}\x1b[0m`)
+      if (((!fs.existsSync(path.join(cwd, "node_modules")))&&(fs.existsSync(path.join(cwd, "package.json"))))) {
+        process.stdout.write(` \x1b[33m(installing)\x1b[0m`)
+        spawnSync("npm", ["install"], {cwd, shell:true})
+      }
+      process.stdout.write(`\r    \x1b[32m${name}\x1b[0m\n`)
+  })
+
+//Start server
+  console.log("\nStarting server :")
+  spawnSync("npm", ["start"], {cwd:path.join(__dirname, "alayer"), shell:true, stdio:[0, 1, 2]})
