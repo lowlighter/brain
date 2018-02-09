d488d40e17570586e0dba0ff4e1f6e36d2c58f0c
Adrylen <adrien.martinez@isen.yncrea.fr>
2018-02-09 11:41:03 +0100

    Add parrot websocket configuration

--- parrot/index.js
+++ parrot/index.js
@@ -1,7 +1,10 @@
-let sumo = require('node-sumo');
+const sumo = require('node-sumo');
+const keypress = require('keypress');
+
+const MAG_Y = 8;
+
 let drone = sumo.createClient();
 
-let keypress = require('keypress');
 keypress(process.stdin);
 
 let forward = drone => {
@@ -16,12 +19,12 @@ let backward = drone => {
 
 let left = drone => {
 	drone.left(20);
-	setTimeout(() => drone.stop(), 200);
+	setTimeout(() => drone.stop(), 1000);
 };
 
 let right = drone => {
 	drone.right(20);
-	setTimeout(() => drone.stop(), 200);
+	setTimeout(() => drone.stop(), 1000);
 };
 
 let tap = drone => {
@@ -33,15 +36,39 @@ let jump = drone => {
 	drone.animationsHighJump();
 }
 
-drone.connect(() => {
-	process.stdin.on('keypress', (ch, key) => {
-		switch(key.name) {
-			case 'z':		forward(drone);	break;
-			case 'q':		left(drone);	break;
-			case 's':		backward(drone);break;
-			case 'd':		right(drone);	break;
-			case 't':		tap(drone);		break;
-			case 'space':	jump(drone);	break;
-		}
-	});
-});
+// drone.connect(() => {
+// 	process.stdin.on('keypress', (ch, key) => {
+// 		switch(key.name) {
+// 			case 'z':		forward(drone);	break;
+// 			case 'q':		left(drone);	break;
+// 			case 's':		backward(drone);break;
+// 			case 'd':		right(drone);	break;
+// 			case 't':		tap(drone);		break;
+// 			case 'space':	jump(drone);	break;
+// 		}
+// 	});
+// });
+
+let start = NaN;
+let delta = 500;
+let samples = [];
+let calibrationSamples = 30;
+
+module.exports = function (callbacks, wss) {
+	drone.connect(() => drone.postureJumper());
+	drone.on('ready', () =>
+		callbacks.mot.push(data => {
+			if(samples.length < calibrationSamples)
+				samples.push(data.mot[MAG_Y])
+			else 
+				if(start === NaN)
+					start = samples.reduce((a,b) => a + b) / calibrationSamples;
+				if(Math.abs(data.mot[MAG_Y] - start) > delta)
+					if(data.mot[MAG_Y] - start > 0)
+						null
+					else
+						null
+		})
+	);
+	return drone;
+};
