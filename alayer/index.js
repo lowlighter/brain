5020cff2af2e09b85c0562903624cb854fe95eed
Lecoq Simon <simon.lecoq@live.fr>
2018-02-10 15:26:54 +0100

    Updated alayer console display

--- alayer/index.js
+++ alayer/index.js
@@ -18,15 +18,43 @@
     met:[event => wss.clients.forEach(ws => { if (ws.readyState === WebSocket.OPEN) ws.send(JSON.stringify(['met', ...event.met])) })]
   }
 
+  const hardware = ["INSIGHT-56A88E2E", "INSIGHT-56A88E44"]
 
-//Error Handling
-  let crashed = false, reconnect = null
-  process.on('uncaughtException', error => {
-    if (!crashed) process.stdout.write(`\x1b[31m\nError : An error occured (${error}), however server should still be active\x1b[0m`)
-    crashed = true
-    clearTimeout(reconnect)
-    reconnect = setTimeout(connect, 5000)
-  })
+//Status
+  const status = {
+    server:false,
+    socket:0,
+    headsets:[],
+    parrot:false,
+    time:0,
+    init() {
+      process.stdout.write(`| # | ${'Server'.padEnd(10)} | ${'Sockets'.padEnd(10)} | ${'Headsets'.padEnd(11)} | ${'Parrot'.padEnd(11)} |\n`)
+      process.stdout.write(`+---+${'-'.repeat(12)}+${'-'.repeat(12)}+${'-'.repeat(13)}+${'-'.repeat(13)}+\n`)
+      status.update()
+      setInterval(() => status.update(), 500)
+    },
+    update() {
+      let c = ["–", "\\", "|", "/"][++status.time%4]
+      let server = `\x1b[${status.server ? 32 : 31}m${(status.server ? "Active" : "Pending").padEnd(10)}\x1b[0m`
+      let socket = `\x1b[${status.socket ? 32 : 31}m${(`${status.socket} client${status.socket > 1 ? 's' : ''}`).padEnd(10)}\x1b[0m`
+      let headset = [`\x1b[${status.headsets.indexOf(hardware[0]) >= 0 ? 32 : 31}m${hardware[0].slice(-4)}\x1b[0m`, `\x1b[${status.headsets.indexOf(hardware[1]) >= 0 ? 32 : 31}m${hardware[1].slice(-4)}\x1b[0m`]
+      let parrot = `\x1b[${status.parrot ? 32 : 31}m${(status.parrot ? "Available" : "Unavailable").padEnd(11)}\x1b[0m`
+      process.stdout.write(`\r| ${c} | ${server} | ${socket} | ${headset[0]} | ${headset[1]} | ${parrot} |`)
+    }
+  }
+
+//Global Error Handling
+  {
+    let reconnect = null
+    process.on('uncaughtException', error => {
+      clearTimeout(reconnect)
+      reconnect = setTimeout(connect, 5000)
+    })
+  }
+
+  status.init()
+
+
 
 //Static server
   app.use('/battle1', express.static(path.join(__dirname, '../battle1')))
@@ -37,14 +65,16 @@
   app.use('/parrot', express.static(path.join(__dirname, '../parrot')))
   app.use('/', express.static(path.join(__dirname, './client')))
 
-  app.listen(3000, () => process.stdout.write('Server started on port 3000\nWebSocket server started on port 3001\n'))
+  app.listen(3000, () => { status.server = true ; status.socket = 0 })
 
 //WebSockets
   wss.on('connection', ws => {
     //Log
-      process.stdout.write('\x1b[36m\nClient logged to wss\x1b[0m')
+      ++status.socket
     //Websockets events
+      ws.on('close', () => --status.socket)
       ws.on('error', () => null)
+    //Websocket commands
       ws.on('message', data => {
         const parsed = JSON.parse(data)
         switch (parsed.action) {
@@ -69,7 +99,7 @@
   function connect() {
     client = new Cortex({verbose:1, threshold:0})
     client.ready.then(() => client.init().queryHeadsets().then(headsets => {
-      if (headsets.length || isConnected) { connected(headsets) } else { process.stdout.write(`\r\x1b[35mSearching for an headset [since ${attempt++} sec]${" ".repeat(30)}\x1b[0m`) ; setTimeout(() => { connect(); checkConnection() }, 1000) }
+      if (headsets.length || isConnected) { connected(headsets) } else { status.headset = []; setTimeout(() => { connect(); checkConnection() }, 1000) }
     })).catch(error => null)
   }
 
@@ -90,10 +120,10 @@
 //Connected to Cortex API
   function connected(headsets) {
     client
-      .createSession({status:'open'})
+      .createSession({status:'open'/*, headset:hardware[0]*/})
       .subscribe({streams:['fac', 'dev', 'pow', 'mot', 'sys', 'met']})
       .then(subs => {
-          if ((!subs[0].fac)||(!subs[1].dev)||(!subs[2].pow)||(!subs[3].mot)||(!subs[4].sys)||(!subs[5].met)) return process.stdout.write(`\n\x1b[31mError : Failed to subscribe to required channels\x1b[0m`)
+          if ((!subs[0].fac)||(!subs[1].dev)||(!subs[2].pow)||(!subs[3].mot)||(!subs[4].sys)||(!subs[5].met)) throw new Error("Couldn't subscribe to required channels")
           client.on('fac', event => callbacks.fac.forEach(callback => callback(event)))
           client.on('dev', event => callbacks.dev.forEach(callback => callback(event)))
           client.on('pow', event => callbacks.pow.forEach(callback => callback(event)))
@@ -101,12 +131,8 @@
           client.on('sys', event => callbacks.sys.forEach(callback => callback(event)))
           client.on('met', event => callbacks.met.forEach(callback => callback(event)))
           let time = 1
-          setInterval(() => client.queryHeadsets().then(headsets => {
-            let active = []
-            headsets.forEach(headset => active.push(headset.id))
-            process.stdout.write(`\r\x1b[32mConnected to headset (${active.join()}) [since ${time++} sec]${" ".repeat(30)}\x1b[0m`)
-          }), 1000)
-      }).catch(error => process.stdout.write(`\n\x1b[31mError : Connection to headset failed\x1b[0m`))
+          setInterval(() => client.queryHeadsets().then(headsets => status.connected = headsets), 1000)
+      }).catch(error => null)
   }
 
 //Start
