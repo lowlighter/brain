3aa1c98c8a2fef6cbdc75d7a85d109449fdf368e
Lecoq Simon <simon.lecoq@live.fr>
2018-02-14 14:03:25 +0100

    Updated headset connection

--- alayer/index.js
+++ alayer/index.js
@@ -1,23 +1,21 @@
-
-
 //Dependancies
   process.stdout.write('\x1Bc')
 
 //Hardware and status
   const hardware = process.env.npm_package_config_hardware
-
-  //const parrot = require('./../parrot/index')
-  const status = require('./modules/status')(hardware)
+  const logger = require('./modules/status')(hardware)
+  const status = logger.status
 
 //Sever and app
   const app = require('express')()
   const server = require('./modules/server')(app, status)
   const callbacks = server.callbacks
-  //const parrot_wifi = require('./modules/parrot-wifi')(status)
-  const logs = require('./modules/logs')
+  const parrot_wifi = require('./modules/parrot-wifi')(status)
+
 
 //Cortex API
   const connection = require('./modules/connection')(status)
 
 //Global Error Handling
   process.on('uncaughtException', error => console.error(error))
+  logger.callbacks(callbacks)
