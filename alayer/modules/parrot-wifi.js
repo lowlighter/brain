abd142389cbe045e6820b196de5b3ba30352a6cb
Adrylen <adrien.martinez@isen.yncrea.fr>
2018-02-14 14:57:37 +0100

    Fix scan module error from node-wifi

--- alayer/modules/parrot-wifi.js
+++ alayer/modules/parrot-wifi.js
@@ -1,20 +1,32 @@
 const wifi = require('node-wifi')
 
+let getStatus = networks => {
+	let status = false
+	networks.forEach(network => {
+		if(network.ssid === "Max_1000")
+			status = true
+	})
+	return status
+}
+
 wifi.init({iface:null})
 let scanner = (status) => {
-	wifi.scan((err, networks) => {
-		setTimeout(scanner, 1000, status)
-		if(err) console.log(err)
-		else {
-			let local_status = false
-			networks.forEach(net => {
-				if(net.ssid === "Max_1000") {
-					local_status = true
-				}
-			})
-			status.parrot = local_status
-		}
-	})
+	setTimeout(scanner, 1000, status)
+
+	wifi.scan()
+		.then(networks => status.parrot = getStatus(networks))
+		.catch(err => {
+			status.parrot = false
+			status.parrot_connected = false
+		})
+
+// 	wifi.getCurrentConnections()
+// 		.then(connections => status.parrot_connected = getStatus(connections))
+// 		.catch(err => {
+// 			console.log("CON")
+// 			status.parrot = false
+// 			status.parrot_connected = false
+// 		})
 }
 
 module.exports = scanner;
--- alayer/modules/status.js
+++ alayer/modules/status.js
@@ -5,6 +5,7 @@
     socket:0,
     headsets:[],
     parrot:false,
+    parrot_connected:false,
     time:0,
     init(hardware) {
       status.hardware = hardware
@@ -18,7 +19,7 @@
       let server = `\x1b[${status.server ? 32 : 31}m${(status.server ? "Active" : "Pending").padEnd(10)}\x1b[0m`
       let socket = `\x1b[${status.socket ? 32 : 31}m${(`${status.socket} client${status.socket > 1 ? 's' : ''}`).padEnd(10)}\x1b[0m`
       let headset = [`\x1b[${status.headsets.indexOf(status.hardware[0]) >= 0 ? 32 : 31}m${status.hardware[0].slice(-4)}\x1b[0m`, `\x1b[${status.headsets.indexOf(status.hardware[1]) >= 0 ? 32 : 31}m${status.hardware[1].slice(-4)}\x1b[0m`]
-      let parrot = `\x1b[${status.parrot ? 32 : 31}m${(status.parrot ? "Available" : "Unavailable").padEnd(11)}\x1b[0m`
+      let parrot = `\x1b[${status.parrot ? 32 : 31}m${(status.parrot_connected ? "Connected" : status.parrot ? "Available" : "Unavailable").padEnd(11)}\x1b[0m`
       process.stdout.write(`\r| ${c} | ${server} | ${socket} | ${headset[0]} | ${headset[1]} | ${parrot} |`)
     }
   }
