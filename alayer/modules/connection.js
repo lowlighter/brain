010ee176e426f1a397173a4b82be26bee092b51a
bloudman <adriencom@msn.com>
2018-02-21 14:04:35 +0100

    trainnig presque fini

--- alayer/modules/connection.js
+++ alayer/modules/connection.js
@@ -19,7 +19,7 @@
     interval = setInterval(() => client.queryHeadsets().then(headsets => status.headsets = headsets.map(h => h.id.toLocaleUpperCase())), 1000)
     client
       .createSession({status:'open'/*, headset:hardware[0]*/})
-      .subscribe({streams:['fac', 'dev', 'pow', 'mot', 'sys', 'met']})
+      .subscribe({streams:['fac', 'dev', 'pow', 'mot', 'sys', 'met', 'com']})
       .then(subs => {
           sid = subs.sid
           if ((!subs[0].fac)||(!subs[1].dev)||(!subs[2].pow)||(!subs[3].mot)||(!subs[4].sys)||(!subs[5].met)) throw new Error("Couldn't subscribe to required channels")
@@ -29,6 +29,7 @@
           client.on('mot', event => callbacks.mot.forEach(callback => callback(event)))
           client.on('sys', event => callbacks.sys.forEach(callback => callback(event)))
           client.on('met', event => callbacks.met.forEach(callback => callback(event)))
+          client.on('com', event => callbacks.com.forEach(callback => callback(event)))
       }).catch(error => null)
   }
 
