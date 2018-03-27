ebe550a62aad9aea508fe7839638c633ae1fcb53
Lecoq Simon <simon.lecoq@live.fr>
2018-03-27 04:17:40 +0200

    Updated alayer server interface

--- alayer/index.js
+++ alayer/index.js
@@ -5,26 +5,29 @@
     if (/server=[0-9/.:a-zA-Z]+/.test(v)) { remote = v.match(/server=([0-9/.:a-zA-Z]+)/)[1] }
   })
 
+//Server instance id
+  const id = require('./modules/id')()
+
 //Hardware and status
   const hardware = process.env.npm_package_config_hardware||["INSIGHT-5A688E2E", "INSIGHT-5A688E44"]
-  const logger = require('./modules/status')(hardware)
+  const logger = require('./modules/status')(hardware, id)
   const status = logger.status
 
 //Sever and app
   const app = require('express')()
-  const server = require('./modules/server')(app, status, remote)
+  const server = require('./modules/server')(app, status, remote, id)
   const callbacks = server.callbacks
   const parrot_wifi = require('./modules/parrot-wifi')(status)
 
 
 //Cortex API
-  const connection = require('./modules/connection')(status, callbacks, hardware).then(d => {
+  const connection = require('./modules/connection')(status, callbacks, hardware, id).then(d => {
     server.client(d.client, d.sid)
   })
 
 //Global Error Handling
   process.on('uncaughtException', error => {
-    if(!/TypeError: Cannot read property 'match' of undefined/.test(error))
-      console.error(error)
+    //if(!/TypeError: Cannot read property 'match' of undefined/.test(error))
+    //  console.error(error)
   })
   logger.callbacks(callbacks)
