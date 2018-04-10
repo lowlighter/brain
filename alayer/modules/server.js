89aa67da721c44eeb0a97f1b2f01aabfb728db08
Lecoq Simon <simon.lecoq@live.fr>
2018-04-10 23:40:43 +0200

    Added shifumi

--- alayer/modules/server.js
+++ alayer/modules/server.js
@@ -36,6 +36,7 @@
       app.use('/recording', express.static(path.join(__dirname, '../../recording')))
       app.use('/training', express.static(path.join(__dirname, '../../training')))
       app.use('/training2', express.static(path.join(__dirname, '../../training2/client')))
+      app.use('/shifumi', express.static(path.join(__dirname, '../../shifumi')))
       app.use('/static', express.static(path.join(__dirname, '../../miscelleanous/static')))
       app.use('/', express.static(path.join(__dirname, './../client')))
 
