c20e9f854631d47ede86cb2c41b759b47c33433b
Lecoq Simon <simon.lecoq@live.fr>
2018-04-11 18:38:55 +0200

    Updated pong calibration

--- pong/client/pong/pong.js
+++ pong/client/pong/pong.js
@@ -449,8 +449,8 @@ Pong = {
       }
     },
 
-    moveUp:         function() { this.up   = 1; },
-    moveDown:       function() { this.down = 1; },
+    moveUp:         function() { this.up   = 2; },
+    moveDown:       function() { this.down = 2; },
     stopMovingUp:   function() { this.up   = 0; },
     stopMovingDown: function() { this.down = 0; }
 
